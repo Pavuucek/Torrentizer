a5ad9cedbe317d02882f02651445522d9ed69ce0
Michal Kuncl <michal.kuncl@gmail.com>
2015-03-31 11:48:56 +0200

    initial tests (assess if this is doable at all)

--- MainWindow.cs
+++ MainWindow.cs
@@ -6,6 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using MonoTorrent;
+using MonoTorrent.BEncoding;
+using MonoTorrent.Common;
 
 namespace Torrentizer
 {
@@ -14,6 +17,24 @@ namespace Torrentizer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var t = new TorrentCreator();
+            t.CreatedBy = "torrentizer pyčo!";
+
+            t.Comment = "komment pyčo!";
+            t.PieceLength = 128*1024;
+            t.Publisher = "publisher";
+            t.StoreMD5 = true;
+            t.Announces.Add(new RawTrackerTier(new string[] {"tier1.anounce1", "tier1.nounce2"}));
+            t.Announces.Add(new RawTrackerTier(new string[] {"tier2.announce1"}));
+
+            var soubory = new TorrentFileSource(".\\");
+            soubory.TorrentName = "bezejmeeeeenaaaaaa";
+            t.Create(soubory, "a.torrent");
+            MessageBox.Show("a");
         }
     }
 }
