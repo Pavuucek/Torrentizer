6d427099b641b151f6ef94847323443534f14ee5
Michal Kuncl <michal.kuncl@gmail.com>
2015-04-08 22:06:51 +0200

    localize everything to english (and back to czech)

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -67,19 +67,19 @@
             // 
             // groupSource
             // 
+            resources.ApplyResources(this.groupSource, "groupSource");
             this.groupSource.Controls.Add(this.comboAdd);
             this.groupSource.Controls.Add(this.labelSkip);
             this.groupSource.Controls.Add(this.textSkip);
             this.groupSource.Controls.Add(this.buttonAddFolder);
             this.groupSource.Controls.Add(this.buttonAddFile);
-            resources.ApplyResources(this.groupSource, "groupSource");
             this.groupSource.Name = "groupSource";
             this.groupSource.TabStop = false;
             // 
             // comboAdd
             // 
-            this.comboAdd.FormattingEnabled = true;
             resources.ApplyResources(this.comboAdd, "comboAdd");
+            this.comboAdd.FormattingEnabled = true;
             this.comboAdd.Name = "comboAdd";
             // 
             // labelSkip
@@ -108,8 +108,8 @@
             // 
             // groupTrackers
             // 
-            this.groupTrackers.Controls.Add(this.textTrackers);
             resources.ApplyResources(this.groupTrackers, "groupTrackers");
+            this.groupTrackers.Controls.Add(this.textTrackers);
             this.groupTrackers.Name = "groupTrackers";
             this.groupTrackers.TabStop = false;
             // 
@@ -120,8 +120,8 @@
             // 
             // groupWebSeeds
             // 
-            this.groupWebSeeds.Controls.Add(this.textWebSeeds);
             resources.ApplyResources(this.groupWebSeeds, "groupWebSeeds");
+            this.groupWebSeeds.Controls.Add(this.textWebSeeds);
             this.groupWebSeeds.Name = "groupWebSeeds";
             this.groupWebSeeds.TabStop = false;
             // 
@@ -132,10 +132,10 @@
             // 
             // groupProperties
             // 
+            resources.ApplyResources(this.groupProperties, "groupProperties");
             this.groupProperties.Controls.Add(this.checkPrivate);
             this.groupProperties.Controls.Add(this.labelPieceLength);
             this.groupProperties.Controls.Add(this.comboPieceLength);
-            resources.ApplyResources(this.groupProperties, "groupProperties");
             this.groupProperties.Name = "groupProperties";
             this.groupProperties.TabStop = false;
             // 
@@ -152,6 +152,7 @@
             // 
             // comboPieceLength
             // 
+            resources.ApplyResources(this.comboPieceLength, "comboPieceLength");
             this.comboPieceLength.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboPieceLength.FormattingEnabled = true;
             this.comboPieceLength.Items.AddRange(new object[] {
@@ -167,7 +168,6 @@
             resources.GetString("comboPieceLength.Items9"),
             resources.GetString("comboPieceLength.Items10"),
             resources.GetString("comboPieceLength.Items11")});
-            resources.ApplyResources(this.comboPieceLength, "comboPieceLength");
             this.comboPieceLength.Name = "comboPieceLength";
             // 
             // progressBar1
@@ -184,13 +184,13 @@
             // 
             // groupRelated
             // 
+            resources.ApplyResources(this.groupRelated, "groupRelated");
             this.groupRelated.Controls.Add(this.labelRelatedTorrents);
             this.groupRelated.Controls.Add(this.textRelatedTorrents);
             this.groupRelated.Controls.Add(this.labelRss);
             this.groupRelated.Controls.Add(this.labelWeb);
             this.groupRelated.Controls.Add(this.textRss);
             this.groupRelated.Controls.Add(this.textWeb);
-            resources.ApplyResources(this.groupRelated, "groupRelated");
             this.groupRelated.Name = "groupRelated";
             this.groupRelated.TabStop = false;
             // 
@@ -226,8 +226,8 @@
             // 
             // groupComment
             // 
-            this.groupComment.Controls.Add(this.textComment);
             resources.ApplyResources(this.groupComment, "groupComment");
+            this.groupComment.Controls.Add(this.textComment);
             this.groupComment.Name = "groupComment";
             this.groupComment.TabStop = false;
             // 
@@ -242,6 +242,7 @@
             // 
             // dialogAddFolder
             // 
+            resources.ApplyResources(this.dialogAddFolder, "dialogAddFolder");
             this.dialogAddFolder.ShowNewFolderButton = false;
             // 
             // dialogSaveTorrent
