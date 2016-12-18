9480240d75e285c28b0e6e3d68a3be4aa285efa7
Michal Kuncl <michal.kuncl@gmail.com>
2016-12-18 20:28:55 +0100

    _build.cmd: modify to use visual studio 2015

--- _build.cmd
+++ _build.cmd
@@ -1,11 +1,9 @@
 @echo off
-set DOTNET_HOME=C:\Windows\Microsoft.NET\Framework\v4.0.30319
 set COMMON_BUILD_OPTIONS=/target:Build
-::/verbosity:detailed
-:: build debug + release
-call "%DOTNET_HOME%\MSBuild" "%~pd0.\Torrentizer.sln" %COMMON_BUILD_OPTIONS% /m /property:Configuration=Debug /property:Platform="Any CPU"
+call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
+MSBuild "%~pd0.\Torrentizer.sln" %COMMON_BUILD_OPTIONS% /m /property:Configuration=Debug /property:Platform="Any CPU"
 if errorlevel 1 goto batch_failed
-call "%DOTNET_HOME%\MSBuild" "%~pd0.\Torrentizer.sln" %COMMON_BUILD_OPTIONS% /m /property:Configuration=Release /property:Platform="Any CPU"
+MSBuild "%~pd0.\Torrentizer.sln" %COMMON_BUILD_OPTIONS% /m /property:Configuration=Release /property:Platform="Any CPU"
 if errorlevel 1 goto batch_failed
 call GitVersioner.bat ba
 goto batch_ok
