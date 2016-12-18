3d51ff3c840b5cbc682c58580be726bf12e8ad06
Michal Kuncl <michal.kuncl@gmail.com>
2016-12-18 20:43:55 +0100

    _build.cmd: update to support source analysis

--- _build.cmd
+++ _build.cmd
@@ -1,8 +1,10 @@
 @echo off
 set COMMON_BUILD_OPTIONS=/target:Build
 call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
+if exist analyze.start.bat call analyze.start.bat
 MSBuild "%~pd0.\Torrentizer.sln" %COMMON_BUILD_OPTIONS% /m /property:Configuration=Debug /property:Platform="Any CPU"
 if errorlevel 1 goto batch_failed
+if exist analyze.end.bat call analyze.end.bat
 MSBuild "%~pd0.\Torrentizer.sln" %COMMON_BUILD_OPTIONS% /m /property:Configuration=Release /property:Platform="Any CPU"
 if errorlevel 1 goto batch_failed
 call GitVersioner.bat ba
